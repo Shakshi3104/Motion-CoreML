5771aac5bdb5edd134c360318c32aa9433b6929c
Shakshi3104 <pikanchisato@yahoo.co.jp>
2022-01-14 18:08:48 +0900

    Initial Commit

---
+++ Motion+CoreML/Motion_CoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Motion_CoreMLApp.swift
+//  Motion+CoreML
+//
+//  Created by MacBook Pro on 2022/01/14.
+//
+
+import SwiftUI
+
+@main
+struct Motion_CoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
